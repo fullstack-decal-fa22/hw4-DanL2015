07b16c12187cc8da9177eb5ab326e0fa060b5d0c
Daniel Liu <daniel17liu@gmail.com>
2022-10-18 13:49:18 -0700

    initial commit

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,23 +1,27 @@
-import { useState } from 'react';
-import React from 'react';
-import Menu from './Menu';
-import Block from './Block';
+import { useState } from "react";
+import React from "react";
+import Menu from "./Menu";
+import Block from "./Block";
 
 const Feed = () => {
-    /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
-    // Refer to Hint 2 for more help!
+  /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
+  // Refer to Hint 2 for more help!
 
-    /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+  /* Use the map() function to render multiple Blocks! */
+  const [posts, setPosts] = useState([]);
 
-    return (
-        <div>
-            <Menu></Menu>
+  function addPost(x) {
+    setPosts([...posts, x]);
+  }
 
-            {/* Below is where all of your Blocks should render! */}
-            {posts}
-        </div>
-    );
-}
+  return (
+    <div>
+      <Menu addPost={addPost}></Menu>
+      {posts.map((item, i) => (
+        <Block color={item}></Block>
+      ))}
+    </div>
+  );
+};
 
 export default Feed;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Color from './Color'
+import Block from './Block'
+
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color handleClick={() => { props.addPost("red") }} color="red"></Color>
+          <Color handleClick={() => { props.addPost("orange") }} color="orange"></Color>
+          <Color handleClick={() => { props.addPost("yellow") }} color="yellow"></Color>
       </div>
     );
 }
